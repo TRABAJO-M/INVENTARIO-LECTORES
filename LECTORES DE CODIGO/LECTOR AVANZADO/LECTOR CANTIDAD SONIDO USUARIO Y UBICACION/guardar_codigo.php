49c04fd868f83aaa72400a494112092ed4281051
MoscheRGD <mosche@outlook.com>
2025-06-16 10:04:09 -0400

    Respaldo del nuevo lector

--- LECTORES DE CODIGO/LECTOR AVANZADO/LECTOR CANTIDAD SONIDO USUARIO Y UBICACION/guardar_codigo.php
+++ LECTORES DE CODIGO/LECTOR AVANZADO/LECTOR CANTIDAD SONIDO USUARIO Y UBICACION/guardar_codigo.php
@@ -1,24 +1,30 @@
 <?php
 date_default_timezone_set("America/Santiago");
+// SECCION PARA INCLUIR LOS MENSAJES QUE LLEGARAN DESDE HTML
 
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $codigo = trim($_POST["codigo"] ?? '');
-    $cantidad = trim($_POST["cantidad"] ?? '');
-    $sector = preg_replace('/[^a-zA-Z0-9_-]/', '', $_POST["sector"] ?? '');
+    $sector = trim($_POST["sector"] ?? '');
+    $bodega = trim($_POST["bodega"] ?? '');
     $usuario = preg_replace('/[^a-zA-Z0-9_-]/', '', $_POST["usuario"] ?? '');
+    $cantidad = trim($_POST["cantidad"] ?? '');
+// FIN SECCION PARA INCLUIR LOS MENSAJES QUE LLEGARAN DESDE HTML
 
-    $usuariosValidos = ["Pedro", "Maria", "Juana"];
-    $sectoresValidos = ["Sector1", "Sector2", "Sector3"];
+// SECCION PARA INCLUIR LOS USUARIOS BODEGA Y RACKS
+$usuariosValidos = ["Cristobal", "Ruben", "Andrea", "Juan", "Sebastian", "Jhoan", "Matias","Claudio", "Renato"];
+$bodegasValidas = ["Primer Piso", "Segundo Piso", "Bodega G3", "Pintura y Lubricante", "Vidrios", "Container", "Vulcanizacion"];
+$sectoresValidos = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "Piso o Pared"];
+// FIN SECCION PARA INCLUIR LOS USUARIOS BODEGA Y RACKS
 
-    if (!in_array($usuario, $usuariosValidos) || !in_array($sector, $sectoresValidos)) {
+    if (!in_array($usuario, $usuariosValidos) || !in_array($bodega, $bodegasValidas) || !in_array($sector, $sectoresValidos) || !is_numeric($cantidad) || $cantidad <= 0) {
         http_response_code(403);
-        exit("Usuario o sector no permitido.");
+        exit("Usuario, bodega o rack no permitido.");
     }
-
-    if ($codigo !== '' && $cantidad !== '') {
+// SECCION PARA CORREGIR FORMATO DEL MENSAJE QUE LLEGARA AL ARCHIVO DE NOTAS
+    if ($codigo !== '') {
         $fecha = date("d/m/Y H:i:s");
-        $linea = "$fecha | Usuario: $usuario | Sector: $sector | Código: *$codigo | Cantidad: *$cantidad" . PHP_EOL;
-
+        $linea = "Código: *$codigo* | Cantidad: *$cantidad* Fecha: *$fecha* | Usuario: *$usuario* | Bodega: *$bodega* | Rack: *$sector*" . PHP_EOL;
+// FIN SECCION PARA CORREGIR FORMATO DEL MENSAJE QUE LLEGARA AL ARCHIVO DE NOTAS
         $archivo = "registros/codigos.txt";
         if (!is_dir("registros")) {
             mkdir("registros", 0777, true);
